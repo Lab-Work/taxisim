ee67a4dd5467e18c3ea8e2ba50ffeea07e28b7dc
Yalan Meng <mengyalan@gmail.com>
2014-09-25 14:49:44 -0500

    Refactored arcFlagsPreProcess.py to fit the PEP 8 style guide.

--- arcFlagsPreProcess.py
+++ arcFlagsPreProcess.py
@@ -1,75 +1,75 @@
-from node import getCorrectNodes
+from node import get_correct_nodes
 from dijkstrasAlgorithm import dijkstra
 import csv
 import timeit
-import time
 
-#Resets the arcflag set in dijkstras algorithm
-def resetArcFlags(gridOfNodes):
-	for column in gridOfNodes:
-		for region in column:
-			for node in region.nodes:
-				for connection in node.isArcFlags:
-					node.isArcFlags[connection] = False
 
-#Converts an arcflag binary string to hexadecimal so that it can be stored in text easier
-def convertToHex(listOfArcs):
-	finalString = ""
-	for i in range(0, len(listOfArcs), 4):
-		valueToHex = 8 * listOfArcs[i] + 4 * listOfArcs[i + 1] + 2 * listOfArcs[i + 2] + listOfArcs[i + 3]
-		finalString += str(hex(valueToHex))[-1:]
-	return finalString
-		
+# Resets the arcflag set in dijkstra's algorithm
+def reset_arc_flags(grid_of_nodes):
+    for _column in grid_of_nodes:
+        for region in _column:
+            for _node in region.nodes:
+                for _connection in _node.is_arc_flags:
+                    _node.is_arc_flags[_connection] = False
 
-gridOfNodes = getCorrectNodes(20, "speeds_per_hour/0_0", None)
+
+# Converts an arcflag binary string to hexadecimal so that it can be stored in text easier
+def convert_to_hex(list_of_arcs):
+    final_string = ""
+    for k in range(0, len(list_of_arcs), 4):
+        value_to_hex = 8 * list_of_arcs[k] + 4 * list_of_arcs[k + 1] + 2 * list_of_arcs[k + 2] + \
+                       list_of_arcs[k + 3]
+        final_string += str(hex(value_to_hex))[-1:]
+    return final_string
+
+
+_grid_of_nodes = get_correct_nodes(20, "speeds_per_hour/0_0", None)
 currSet = set()
 i = 0
 dictionaryOfArcFlags = dict()
 fastestVelocity = 0
-for column in gridOfNodes:
-	for gridRegion in column:
-		for node in gridRegion.nodes:
-			for connection in node.isArcFlags:
-				dictionaryOfArcFlags[(str(node.id), str(connection.id))] = [0] * 400
-				if node.speedConnections[connection] > fastestVelocity:
-					fastestVelocity = node.speedConnections[connection]
+for column in _grid_of_nodes:
+    for grid_region in column:
+        for node in grid_region.nodes:
+            for connection in node.is_arc_flags:
+                dictionaryOfArcFlags[(str(node.id), str(connection.id))] = [0] * 400
+                if node.speed_connections[connection] > fastestVelocity:
+                    fastestVelocity = node.speed_connections[connection]
 start = timeit.default_timer()
-for column in gridOfNodes:
-	for gridRegion in column:
-		print "Next Region!"
-		if i % 10 == 0:
-			stop = timeit.default_timer()
-			print stop - start
-		setOfNodes = set()
-		#Makes sure setOfNodes only contains the boundaryNodes
-		for node in gridRegion.nodes:
-			if node.isBoundaryNode:
-				setOfNodes.add(node)
-	
-		start = timeit.default_timer()
-		#Does a multi-dijkstra search to get an arcflag tree
-		dijkstra(setOfNodes, gridOfNodes)
-		for column in gridOfNodes:
-			for gridRegion in column:
-				for node in gridRegion.nodes:
-					for connection in node.isArcFlags:
-						if node.isArcFlags[connection]:
-							#A new arcFlag entry - startNode, endNode, region arcflags go to)
-							dictionaryOfArcFlags[(str(node.id), str(connection.id))][i] = 1
-		resetArcFlags(gridOfNodes)
-		i += 1
-linkFile = csv.writer(open("arcFlags/20Regions0_0.csv", 'wb'))
-headers = ["startNodeID", "endNodeID", "hexStringOfRegions"] #This is a hexadecimal string that converts region to true or false 
-#RegionNumber = 0, 1, 2, 3, 4, 5, 6, 7
-#isArcFlags   = 0, 1, 1, 0, 1, 1, 0, 1
-#HexString	  = 6D
+for column in _grid_of_nodes:
+    for grid_region in column:
+        print "Next Region!"
+        if i % 10 == 0:
+            stop = timeit.default_timer()
+            print stop - start
+        set_of_nodes = set()
+        # Makes sure setOfNodes only contains the boundaryNodes
+        for node in grid_region.nodes:
+            if node.is_boundary_node:
+                set_of_nodes.add(node)
 
-linkFile.writerow(headers)
+        start = timeit.default_timer()
+        # Does a multi-dijkstra search to get an arcflag tree
+        dijkstra(set_of_nodes, _grid_of_nodes)
+        for col in _grid_of_nodes:
+            for _grid_region in col:
+                for node in _grid_region.nodes:
+                    for connection in node.is_arc_flags:
+                        if node.is_arc_flags[connection]:
+                            # A new arcFlag entry - startNode, endNode, region arcflags go to)
+                            dictionaryOfArcFlags[(str(node.id), str(connection.id))][i] = 1
+        reset_arc_flags(_grid_of_nodes)
+        i += 1
+link_file = csv.writer(open("arcFlags/20Regions0_0.csv", 'wb'))
+# This is a hexadecimal string that converts region to true or false
+headers = ["startNodeID", "endNodeID", "hexStringOfRegions"]
+# RegionNumber = 0, 1, 2, 3, 4, 5, 6, 7
+#is_arc_flags   = 0, 1, 1, 0, 1, 1, 0, 1
+#HexString      = 6D
+
+link_file.writerow(headers)
 for key in dictionaryOfArcFlags:
-	currList = dictionaryOfArcFlags[key]
-	hexString = convertToHex(currList)
-	newArr = []
-	newArr.append(key[0])
-	newArr.append(key[1])
-	newArr.append(hexString)
-	linkFile.writerow(newArr)
+    curr_list = dictionaryOfArcFlags[key]
+    hex_string = convert_to_hex(curr_list)
+    new_arr = [key[0], key[1], hex_string]
+    link_file.writerow(new_arr)
