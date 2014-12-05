d978edbf845835b1b3d3eced4d87acb728eb56b2
Yalan Meng <mengyalan@gmail.com>
2014-12-05 14:42:56 -0600

    Initial KDTree stuff

---
+++ Map.py
@@ -0,0 +1,22 @@
+'''
+Created on Dec 5, 2014
+
+@author: mengyalan
+'''
+
+from kdtree import KDTree
+
+
+class Map:
+    def get_nearest_node(self, lat, lon):
+        pass
+
+    def get_node_region(self, node):
+        pass
+
+    def __init__(self, nodes_fn, links_fn):
+        # node_list
+        # link_list
+        # region_kd
+        # nearest_kd
+        pass
