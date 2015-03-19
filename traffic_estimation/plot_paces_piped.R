9191dead0baea4384090db6858339a951b8d6c6a
Brian Donovan <briandonovan100@gmail.com>
2015-03-18 22:26:28 -0500

    Updated video generation to show pace instead of velocity

--- traffic_estimation/plot_paces_piped.R
+++ traffic_estimation/plot_paces_piped.R
@@ -9,12 +9,10 @@ lon_meters = 84253.1418965
 
 
 #jet.colors =colorRamp(c("#00007F", "blue", "#007FFF", "cyan",
-#                     "#7FFF7F", "yellow", "#FF7F00", "red"))
+#                     "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
 
-jet.colors =colorRamp(c("blue", "#007FFF", "cyan",
-                     "#7FFF7F", "yellow", "#FF7F00", "red"))
-
-
+jet.colors =colorRamp(rev(c("blue", "#007FFF", "cyan",
+                     "#7FFF7F", "yellow", "#FF7F00", "red")))
 
 
 linearScale = function(x, lo, hi){
@@ -31,17 +29,15 @@ argv = commandArgs(trailingOnly=T)
 outfile = argv[1]
 my_title = argv[2]
 plot_type = argv[3]
-print(plot_type)
 
 if(plot_type=="absolute"){
-	print("ASDFASF")
-	lo_pace = 1
-	hi_pace = 15
+	lo_speed_mph=0
+	hi_speed_mph=35
 	value_granularity=10
-	legend_granularity=1
+	legend_granularity=5
 } else if(plot_type=="zscore"){
-	lo_pace=-5
-	hi_pace=5
+	lo_speed_mph=-5
+	hi_speed_mph=5
 	value_granularity=30
 	legend_granularity=1
 }
@@ -55,12 +51,12 @@ if(plot_type=="absolute"){
 #print(paste("Reading", infile))
 
 png(outfile, 1024, 1024)
-par(mar=c(0,2,4,2), family="sans")
+par(mar=c(2,5,3,2), family="sans", bg="black", col.axis="white",col.lab="white", fg="white")
 layout(c(1,2), heights=c(7,1))
 
 
 t = read.csv('stdin')
-
+#t = t[t$speed < 6.915 | t$speed > 6.916,]
 
 t$start_lat = t$start_lat * lat_meters
 t$end_lat = t$end_lat * lat_meters
@@ -74,12 +70,11 @@ t$end_lon = t$end_lon * lon_meters
 
 #print("Computing colors")
 #summary(t$speed)
-t$pace[is.na(t$pace)] = 0
+t$speed[is.na(t$speed)] = 0
 
-#convert the pace to minutes per mile
-t$pace = t$pace * 26.8224
+speed_mph = t$speed * 2.23694
 
-cvals = linearScale(t$pace, lo_pace, hi_pace)
+cvals = linearScale(speed_mph, lo_speed_mph, hi_speed_mph)
 cols = rgb(jet.colors(cvals)/255)
 #cols=rgb(jet.colors(t$speed/31)/255)
 
@@ -95,10 +90,6 @@ plot(clon,clat,type="n",
 	xlim=c(clon*lon_meters-size,clon*lon_meters+size),
 	ylim=c(clat*lat_meters-size, clat*lat_meters+size),
 	xaxt="n", yaxt="n", xlab="", ylab="")
-abline(v = clon*lon_meters, col = "black", lwd = 2000)
-
-
-
 segments(t$start_lon, t$start_lat, x1=t$end_lon, y1=t$end_lat, col=cols, lwd=3)
 
 
@@ -106,19 +97,19 @@ segments(t$start_lon, t$start_lat, x1=t$end_lon, y1=t$end_lat, col=cols, lwd=3)
 
 
 
-par(mar=c(0,1,3,1))
-title(main=my_title, cex.main=3)
+par(mar=c(1,1,3,1))
+title(main=my_title, cex.main=3, col.main="white")
 
 
 print("Adding legend")
-plot(0,0, type="n", xlim=c(lo_pace,hi_pace), ylim=c(0,1), xaxt="n", yaxt="n", , bty="n", cex.main=.8, main="")
+plot(0,0, type="n", xlim=c(lo_speed_mph,hi_speed_mph+1), ylim=c(0,1), xaxt="n", yaxt="n", , bty="n", cex.main=.8, main="")
 
 
 	
-	vals = seq(lo_pace, hi_pace, 1/value_granularity)
-	cvals = linearScale(vals, lo_pace, hi_pace)
+	vals = seq(lo_speed_mph, hi_speed_mph, 1/value_granularity)
+	cvals = linearScale(vals, lo_speed_mph, hi_speed_mph)
 	mycols = rgb(jet.colors(cvals)/255)
-	a = c((0:((hi_pace - lo_pace)/legend_granularity))*value_granularity*legend_granularity + 1)
+	a = c((0:((hi_speed_mph - lo_speed_mph)/legend_granularity))*value_granularity*legend_granularity + 1)
 
 
 
@@ -132,13 +123,13 @@ plot(0,0, type="n", xlim=c(lo_pace,hi_pace), ylim=c(0,1), xaxt="n", yaxt="n", , 
 	#axis(1, at=a, labels=round(vals[a], 2))
 	#abline(v=a)
 	if(plot_type=="absolute"){
-		units = "Pace (min/mi)"
+		units = "Velocity (mph)"
 	} else if(plot_type=="zscore"){
-		units = "Pace Z-Score"
+		units = "Velocity Z-Score"
 	}
-	text(x=(hi_pace+lo_pace)/2,y=.9,labels=units, cex=2, col.main="white")
-	segments(x0=vals[a],y0=.4,x1=vals[a],y1=.6, lwd=2)
-	text(x=vals[a],y=.2,labels=vals[a], cex=2, )
+	text(x=(hi_speed_mph+lo_speed_mph)/2,y=.8,labels=units, cex=2, col.main="white")
+	segments(x0=vals[a],y0=.4,x1=vals[a],y1=.6, col="white", lwd=2)
+	text(x=vals[a],y=.2,labels=vals[a], cex=2, col.main="white")
 
 dev.off()
 
